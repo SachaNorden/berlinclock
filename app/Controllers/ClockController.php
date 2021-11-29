b408768b912bbf1e31a23f7f2e842b002be9c117
Sacha Norden <sacha.norden@student.vinci.be>
2021-11-29 22:09:35 +0100

    'essaie-afficher-horloge'

--- app/Controllers/ClockController.php
+++ app/Controllers/ClockController.php
@@ -6,7 +6,10 @@ class ClockController {
     }
 
     public function run() {
-
+        $fivehours=4;
+        $hours=4;
+        $fiveminutes=11;
+        $minutes=4;
         # Le contrôleur d'acceuil se termine en écrivant la vue accueil
         require_once(VIEWS_PATH.'clock.php');
     }
--- app/views/clock.php
+++ app/views/clock.php
@@ -2,4 +2,26 @@
     <h2>Accueil</h2>
     <p>Bienvenue sur l'horloge de berlin</p>
     <p>DEKIMPE Thomas.</p>
+
+    <table>
+        <tr>
+            <td><?php  echo 'Y'?></td>
+        </tr>
+        <tr>
+            <?php  foreach ($fivehours as $i => $value) ?>
+            <td> <?php  echo 'R'?> </td>
+        </tr>
+        <tr>
+            <?php  foreach ($hours as $i => $value) ?>
+            <td> <?php  echo 'R'?> </td>
+        </tr>
+        <tr>
+            <?php  foreach ($fiveminutes as $i => $value) ?>
+            <td> <?php  echo 'R'?> </td>
+        </tr>
+        <tr>
+            <?php  foreach ($minutes as $i => $value) ?>
+            <td> <?php  echo 'R'?> </td>
+        </tr>
+    </table>
 </section>
