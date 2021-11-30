56f830a1010703d215a93cd47d9acf1953261c6e
Sacha Norden <sacha.norden@student.vinci.be>
2021-11-30 18:11:32 +0100

    'ajout-de-l-heure'

--- app/Controllers/ClockController.php
+++ app/Controllers/ClockController.php
@@ -6,10 +6,27 @@ class ClockController {
     }
 
     public function run() {
-        $fivehours=4;
-        $hours=4;
-        $fiveminutes=11;
-        $minutes=4;
+        $seconde=date('s');
+        $hours=date('H');
+        $minutes=date('i');
+        echo ($hours );
+        echo(" ");
+        echo ( $minutes);
+        echo(" ");
+        echo ($seconde);
+
+        $secondeCase='0';
+
+        $fivehoursCase=$hours/5;
+        $fivehoursCase=floor($fivehoursCase);
+
+        $hoursCase=$hours%5;
+
+        $fiveminutesCase=$minutes/5;
+        $fiveminutesCase=floor($fiveminutesCase);
+
+        $minutesCase=$minutes%5;
+
         # Le contrôleur d'acceuil se termine en écrivant la vue accueil
         require_once(VIEWS_PATH.'clock.php');
     }
--- app/views/clock.php
+++ app/views/clock.php
@@ -2,30 +2,31 @@
     <h2>Accueil</h2>
     <p>Bienvenue sur l'horloge de berlin</p>
     <p>DEKIMPE Thomas Norden Sacha .</p>
-
+<div class="center">
     <table>
         <tr>
-            <td><?php  echo 'Y'?></td>
+            <td><?php  echo $secondeCase?></td>
         </tr>
         <tr>
             <?php for($i=0;$i<4;$i++) { ?>
-            <td> <?php  echo 'R'?> </td>
+            <td> <?php  echo $fivehoursCase?> </td>
             <?php } ?>
         </tr>
         <tr>
             <?php for($i=0;$i<4;$i++){ ?>
-            <td> <?php  echo 'R'?> </td>
+            <td> <?php  echo $hoursCase?> </td>
             <?php } ?>
         </tr>
         <tr>
             <?php  for($i=0;$i<11;$i++){ ?>
-            <td> <?php  echo 'R'?> </td>
+            <td> <?php  echo $fiveminutesCase ?> </td>
             <?php } ?>
         </tr>
         <tr>
             <?php  for ($i=0;$i<4;$i++){ ?>
-            <td> <?php  echo 'R'?> </td>
+            <td> <?php  echo $minutesCase?> </td>
             <?php } ?>
         </tr>
     </table>
+</div>
 </section>
