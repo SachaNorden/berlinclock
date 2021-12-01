e1cf6c1923e851d2696767960ca406273698c8bb
Thomas <thomas.dekimpe@student.vinci.be>
2021-12-01 16:44:45 +0100

    On revient à la structure demandée car controller & vue non demandée

--- app/views/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>L'horloge de berlin</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo VIEWS_PATH ?>css/base.css" media="all">
-    
-</head>
-<body>
