5167bf02d51674e9726cdc13f8feda5ffc10630c
Thomas <thomas.dekimpe@student.vinci.be>
2021-12-01 20:28:39 +0100

    add du début classes test

---
+++ BerlinClockTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BerlinClock;
+
+use BerlinClock;
+use PHPUnit\Framework\TestCase;
+
+class BerlinClockTest extends TestCase
+{
+    public function testSeconde()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
