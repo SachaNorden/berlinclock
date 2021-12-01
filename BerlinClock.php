ab180b476e22a6564befd04bbce3e9a47c6c738a
Thomas <thomas.dekimpe@student.vinci.be>
2021-12-01 16:44:58 +0100

    On revient à la structure demandée car controller & vue non demandée

---
+++ BerlinClock.php
@@ -0,0 +1,28 @@
+<?php
+
+$fivehours=4;
+$hours=4;
+$fiveminutes=11;
+$minutes=4;
+
+echo 'Y';
+echo "<br/>";
+
+for($i=0;$i<4;$i++) {
+    echo 'R';
+}
+echo "<br/>";
+
+for($i=0;$i<4;$i++){
+    echo 'R';
+}
+echo "<br/>";
+
+for($i=0;$i<11;$i++){
+    echo 'R';
+}
+echo "<br/>";
+
+for ($i=0;$i<4;$i++){
+    echo 'R';
+}
