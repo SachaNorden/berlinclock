8af21373c6b35cb91e5419bf4cc6ba3b3d78fdde
dekimpethomas1 <thomas.dekimpe@student.vinci.be>
2021-12-02 10:31:07 +0100

    add - test secondes, minutes x5 et minutes simples

--- BerlinClock.php
+++ BerlinClock.php
@@ -2,13 +2,20 @@
 
 class BerlinClock
 {
-
-    private $seconde = true;
+    private $seconde;
     private $fiveHours = 4;
     private $hours = 4;
     private $fiveMinutes = 11;
     private $minutes = 4;
 
+    public function __construct(){
+        $this->seconde = (int)date("s")%2 == 1;
+        $this->fiveHours = floor((int)date("h") / 5);
+        $this->hours = (int)date("h") % 5;
+
+        $this->fiveMinutes = floor((int)date("i") / 5);
+        $this->minutes = (int)date("i") % 5;
+    }
 
     function getSecondes()
     {
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -17,6 +17,32 @@ class BerlinClockTest extends TestCase
         $actual = $clock->getSecondes();
 
         // Assert
-        $this->assertEquals(true, $actual);
+        $this->assertEquals((int)date("s")%2 == 1, $actual);
+    }
+
+    public function testFiveMinutes()
+    {
+        // Arrange
+        $clock = new BerlinClock();
+
+        // Act
+        $actual = $clock->getFiveMinutes();
+        $expected = round((int)date("i") / 5);
+
+        // Assert
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testMinutes()
+    {
+        // Arrange
+        $clock = new BerlinClock();
+
+        // Act
+        $actual = $clock->getMinutes();
+        $expected = round((int)date("i") % 5);
+
+        // Assert
+        $this->assertEquals($expected, $actual);
     }
 }
